b9dcf7332bd3ba5e914469d835655ff15f68d212
ja05178 <ja05178@georgiasouthern.edu>
2019-03-05 19:17:24 -0500

    First Commit

--- app/src/main/java/com/example/shopeasydemo/MainActivity.java
+++ app/src/main/java/com/example/shopeasydemo/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.shopeasydemo;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import org.jsoup.Jsoup;
@@ -11,9 +18,15 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
-    private String text = "abc";
+    private String text;
+    private String [] ingredients;
+    private String url;
+    private Button searchButton;
+    private EditText et;
+    private Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,19 +34,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //TextView textView = (TextView) findViewById(R.id.textView);
         //textView.setText("onCreate");
-        new getData().execute();
-
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
+        et = (EditText) findViewById(R.id.editText);
+        searchButton = (Button) findViewById(R.id.searchButton);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                url = et.getText().toString();
+                text = "";
+                new getData().execute();
+            }
+        });
+        //new getData().execute();
     }
         private class getData extends AsyncTask<Void, Void, Void>{
 
             @Override
             protected Void doInBackground(Void... voids) {
-                String url = "https://www.allrecipes.com/recipe/257938/spicy-thai-basil-chicken-pad-krapow-gai/?internalSource=staff%20pick&referringId=201&referringContentType=Recipe%20Hub";
-
                 try {
                     Document doc = Jsoup.connect(url).get();
-                    Elements ingredient = doc.select("span[class=recipe-ingred_txt added]");
-                    text = ingredient.text();
+                    Elements ingredientElement = doc.select("span[class=recipe-ingred_txt added]");
+                    Element image = doc.select("img.rec-photo").first();
+                    String src = image.attr("src");
+                    InputStream input = new java.net.URL(src).openStream();
+                    bitmap = BitmapFactory.decodeStream(input);
+                    ingredients = new String[ingredientElement.size()];
+                    for(int i =0; i < ingredientElement.size();i++){
+                        //ingredients[i] = ingredientElement.get(i).text();
+                        if(text == null){
+                            text =  ingredientElement.get(i).text();
+                        }else{
+                            text = text +  "\n" +  ingredientElement.get(i).text();
+                        }
+                    }
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -42,6 +75,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             protected void onPostExecute(Void result) {
                 TextView textView = (TextView) findViewById(R.id.textView);
+                ImageView imageView = (ImageView) findViewById(R.id.imageView);
+                imageView.setImageBitmap(bitmap);
                 textView.setText(text);
             }
         }
